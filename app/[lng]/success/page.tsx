dc215a35edd2a748ececc81a99caa4e628055ca2
warlockja <warlockja@gmail.com>
2025-03-26 12:37:03 +0300

    fix build

--- app/[lng]/success/page.tsx
+++ app/[lng]/success/page.tsx
@@ -1,24 +1,30 @@
-'use client'
-import styles from './page.module.scss'
-import DividerLine from '@/components/home/dividers/DividerLine'
-import DividerThiccRed from '@/components/home/dividers/DividerThiccRed'
-import Link from 'next/link'
-import { useTranslation } from '@/app/i18n/client'
-import { paramsType } from '@/index'
+"use client";
+import styles from "./page.module.scss";
+import DividerLine from "@/components/home/dividers/DividerLine";
+import DividerThiccRed from "@/components/home/dividers/DividerThiccRed";
+import Link from "next/link";
+import { useTranslation } from "@/app/i18n/client";
+import { paramsType } from "@/index";
 
-export default function page({ params: { lng } }:paramsType) {
-    // cart cleanup on return from Stripe
-    if (typeof window !== "undefined") localStorage.removeItem('ecom-cart')
+export default function page({ params: { lng } }: paramsType) {
+  // cart cleanup on return from Stripe
+  if (typeof window !== "undefined") localStorage.removeItem("ecom-cart");
 
-    const { t } = useTranslation(lng, 'purchase-success')
+  // eslint-disable-next-line
+  const { t } = useTranslation(lng, "purchase-success");
 
-    return (
-        <div className={styles.purchaseSuccess}>
-            <h1>{t('header')}</h1>
-            <p>{t('flavour-text')}</p>
-            <DividerThiccRed />
-            <DividerLine />
-            <p>{t('disclaimer')} <Link className={styles.Link} href='/feedback'>{t('link')}</Link></p>
-        </div>
-    )
+  return (
+    <div className={styles.purchaseSuccess}>
+      <h1>{t("header")}</h1>
+      <p>{t("flavour-text")}</p>
+      <DividerThiccRed />
+      <DividerLine />
+      <p>
+        {t("disclaimer")}{" "}
+        <Link className={styles.Link} href="/feedback">
+          {t("link")}
+        </Link>
+      </p>
+    </div>
+  );
 }
--- components/utils/StoreInitializer.tsx
+++ components/utils/StoreInitializer.tsx
@@ -1,41 +1,46 @@
-'use client'
+"use client";
 
-import LocalStorageRead from "@/components/utils/LocalStorageRead"
-import { ItemType, CartItemType, LocalStoreDataType } from "@/index"
-import { useStore } from "@/src/store"
-import { useEffect } from "react"
+import LocalStorageRead from "@/components/utils/LocalStorageRead";
+import { ItemType, CartItemType, LocalStoreDataType } from "@/index";
+import { useStore } from "@/src/store";
+import { useEffect } from "react";
 
-export default function StoreInitializer({ shopItems }: { shopItems: ItemType[] }) {
+export default function StoreInitializer({
+  shopItems,
+}: {
+  shopItems: ItemType[];
+}) {
+  useEffect(() => {
+    // reading local storage for saved data
+    const storedData: LocalStoreDataType | undefined = LocalStorageRead();
+    // creating blank template from fetched shop items for the local store
+    const cartItems: CartItemType[] = shopItems.map((item) => {
+      return {
+        id: item.id,
+        image: item.image,
+        width: item.width,
+        height: item.height,
+        price: item.price,
+        title: item.title,
+        description: item.description,
+        amount: 0,
+      };
+    });
+    // populating local store template with local storage data if present
+    if (!storedData) {
+      // creating empty object if no data found
+      useStore.setState({ cartItems, cartTotal: 0 });
+    } else {
+      storedData.cartItems.map((item) => {
+        const cartIndex = cartItems.findIndex(
+          (cartItem) => cartItem.id === item.id
+        );
+        cartItems[cartIndex].amount = item.amount;
+      });
 
-    useEffect(() => {
-        // reading local storage for saved data
-        const storedData: LocalStoreDataType | undefined = LocalStorageRead()
-        // creating blank template from fetched shop items for the local store
-        const cartItems: CartItemType[] = shopItems.map((item) => {
-            return({
-                id: item.id,
-                image: item.image,
-                width: item.width,
-                height: item.height,
-                price: item.price,
-                title: item.title,
-                description: item.description,
-                amount: 0
-            })
-        })
-        // populating local store template with local storage data if present
-        if(!storedData) {
-            // creating empty object if no data found
-            useStore.setState({ cartItems, cartTotal: 0 })
-        } else {
-            storedData.cartItems.map(item => {
-                const cartIndex = cartItems.findIndex(cartItem => cartItem.id === item.id)
-                cartItems[cartIndex].amount = item.amount
-            })
-            
-            useStore.setState({ ...storedData, cartItems: cartItems })
-        }
-    },[])
+      useStore.setState({ ...storedData, cartItems: cartItems });
+    }
+  }, [shopItems]);
 
-    return null
+  return null;
 }
